f92c56ea88c6b6c97a2a23557a67500480242aa3
Nastya <anastasiia.havrylchyk@oa.edu.ua>
2025-04-16 15:00:14 +0300

    Update vite file again

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Lab_5_CI_CD/', // назва вашого репозиторію
+  base: '/lab5-ci-cd/', // назва вашого репозиторію
 });
